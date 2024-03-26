2ad45d32cbc38e91f463e2fe0b8e393b11dc7ad6
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-03-26 10:57:09 +0100

    Implemented method AddUser_ReturnsFalseWhenFirstNameIsEmpty

--- zadanie/LegacyApp.Tests/UserServiceTests.cs
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -5,5 +5,20 @@ public class UserServiceTests
     [Fact]
     public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
     {
+        //Arrange
+        var userService = new UserService();
+
+        //Act
+        var result = userService.AddUser(
+            null,
+            "Kowalski",
+            "kowalski@kowal.com",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+
+        //Assert
+        //Assert.Equal(false, result); - for older versions
+        Assert.False(result);
     }
 }
