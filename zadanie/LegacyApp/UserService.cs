68c1bd04b3372f3b0d5c069e049479228b94993b
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-08 19:52:58 +0200

    Class UserValidator with static method CheckUser responsible for validating the data of the new user

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -6,24 +6,10 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            if (!UserValidator.CheckUser(firstName, lastName, email, dateOfBirth))
             {
                 return false;
-            }
-
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
+            };
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
